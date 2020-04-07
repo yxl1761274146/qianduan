a51dd218a7d65188086c8b7b5d9779e95f818ffa
yxl1761274146 <1761274146@qq.com>
2020-04-07 10:40:46 +0800

    作业4 41-yxl finish

--- post_youdao.py
+++ post_youdao.py
@@ -3,13 +3,22 @@ import requests
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
 
 def get_salt():
-    return '15846844357889'
+    s=str(random.randint(0,10))
+    t=get_ts()
+    # print("random = ",s)
+    # print("ts= ",t)
+    # print("salt= ",t+s)
+    return t+s
+    # return '15846844357889'
 
 def get_sign():
     return  '4cf44da69384da8fb5c2364a31b22380'
 
 def get_ts():
-    return '1584684435788'
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts
+    # '1584684435788'
 form_data={
     'i':'我和你',
     'from': 'AUTO',
