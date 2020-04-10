bf01ecebf9877156caedafb01dba7ec09a26d4e6
yxl1761274146 <1761274146@qq.com>
2020-04-10 19:57:07 +0800

    作业5 41-yxl finish youdao

--- post_youdao.py
+++ post_youdao.py
@@ -1,24 +1,43 @@
+import random
 import requests
+import time
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+content="我和你"
 
 def get_salt():
     s=str(random.randint(0,10))
     t=get_ts()
-    # print("random = ",s)
-    # print("ts= ",t)
-    # print("salt= ",t+s)
-    return t+s
-    # return '15846844357889'
+    #print("random= ",s)
+    #print("ts= ",t)
+    #print("salt ",t+s)
+    return  t+s
+    #return '15864988855634'
+
+def get_md5(value):
+    import hashlib
+    m =hashlib.md5()
+    m.update(value.encode("utf-8"))
+    return m.hexdigest()
 
 def get_sign():
-    return  '4cf44da69384da8fb5c2364a31b22380'
+    i=get_salt()
+    e=get_content()
+    s="fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
+    print("s=",s," md5=",get_md5(s))
+    return get_md5(s)
+    return  '63ec4fac43e6d508ff508ab79a47cd11'
 
 def get_ts():
     t = time.time()
-    ts = str(int(round(t * 1000)))
+    ts=str(int(round(t * 1000)))
     return ts
-    # '1584684435788'
+    # '1585615400595'
+
+def get_content():
+    return content
+
+
 form_data={
     'i':'我和你',
     'from': 'AUTO',
@@ -28,11 +47,31 @@ form_data={
     'salt': get_salt(),
     'sign': get_sign(),
     'ts': get_ts(),
-    'bv': '70244e0061db49a9ee62d341c5fed82a',
+    'bv': 'f0ece28f714caaf3c6cee92884b1a685',
     'doctype': 'json',
     'version': '2.1',
     'keyfrom': 'fanyi.web',
-    'action': 'FY_BY_REALTlME',
+    'action': 'FY_BY_CLICKBUTTION',
 }
-response=requests.post(url,form_data)
-print(response.text)
+
+def get_headers():
+    headers={
+        'Cookie: OUTFOX_SEARCH_USER_ID=197877919@10.108.160.18; JSESSIONID=aaa-1EcV6hVDE9DsYSIfx; OUTFOX_SEARCH_USER_ID_NCOO=196562409'
+        'Referer: http://fanyi.youdao.com/',
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.9 Safari/537.36'
+    }
+    return headers
+
+class Youdao():
+    def __init__(self):
+        self.ts=self.get_ts()
+        self.salt=self.get_salt()
+        self.sign-self.get_sign()
+
+
+if __name__ == '__main__':
+    # ts=get_ts
+    print(form_data)
+    print(get_headers())
+    response=requests.post(url,data=form_data,headers=get_headers())
+    print(response.text)
--- test_post_youdao.py
+++ test_post_youdao.py
@@ -1,16 +1,28 @@
+import unittest
 from unittest import mock
+
+#from post_youdao import *
 
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True,True)
 
     def test_get_ts(self):
-        get_ts=mock.Mok(return_value='1584684435788')
-        self.assertEqual('1584684435788',get_ts())
+        #import time
+        #t=time.time()
+        #ts=str(int(round(t * 1000)))
+        #prrint(ts)
+        get_ts=mock.Mock(return_value='1586498885563')
+        self.assertEqual('1586498885563',get_ts())
 
     def test_get_salt(self):
-        get_salt=mock.Mok(return_value='15846848803956')
-        self.asserEqual('15846848803956',get_salt())
+        get_salt = mock.Mock(return_value='15864988855634')
+        self.assertEqual('15864988855634',get_salt())
+
+    def test_get_sign(self):
+        get_sign=mock.Mock(return_value="")
+        self.assertEqual('63ec4fac43e6d508ff508ab79a47cd11',get_sign())
+
 
 if __name__=='__main__':
     unittest.main()
